8b9966829c12e287cdb92ab712d65121eedbaaad
Celery00 <934740483@qq.com>
2018-01-18 18:02:00 +0800

    Add or update. -- git_shell.sh

--- home/FlappyBird/js/Bird.js
+++ home/FlappyBird/js/Bird.js
@@ -129,7 +129,12 @@
                 self.deleteY = 1;
             }
             
-            
+            var ua = navigator.userAgent.toLowerCase();  
+            if(ua.match(/MicroMessenger/i)=="micromessenger") {  
+                console.log(1)  
+            } else {  
+                console.log(0)   
+            }  
             
             
             // game.canvas.on('click','touch',function(){
