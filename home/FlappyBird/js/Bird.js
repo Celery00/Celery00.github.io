809f73a126cec74616295c4631a0133a3f1142cc
Celery00 <934740483@qq.com>
2018-01-18 17:31:50 +0800

    Add or update. -- git_shell.sh

--- home/FlappyBird/js/Bird.js
+++ home/FlappyBird/js/Bird.js
@@ -119,21 +119,21 @@
         blindClick : function () {
             //备份指针
             var self = this;
-            // var onclick  = window.onclick?'onclick':'touch';
-            // game.canvas.onclick = function () {
-            //     // 更新小鸟的状态
-            //    self.state = 1;
-            //    // 小鸟上升的角度
-            //     self.rotateAngle = -25;
-            //     // 复位空气的阻力
-            //     self.deleteY = 1;
-            // }
-            
-            game.canvas.on('click',function(){
-                self.state = 1;
+            var onclick  = window.onclick?'onclick':'touch';
+            game.canvas.onclick = function () {
+                // 更新小鸟的状态
+               self.state = 1;
+               // 小鸟上升的角度
                 self.rotateAngle = -25;
+                // 复位空气的阻力
                 self.deleteY = 1;
-            })
+            }
+            
+            // game.canvas.on('click',function(){
+            //     self.state = 1;
+            //     self.rotateAngle = -25;
+            //     self.deleteY = 1;
+            // })
         }
         
     });
