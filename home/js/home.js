55e60211844a1f4bac37f11aa9e9f8fbb355686d
Celery00 <934740483@qq.com>
2018-01-19 17:03:36 +0800

    Add or update. -- git_shell.sh

--- home/js/home.js
+++ home/js/home.js
@@ -1,8 +1,24 @@
 
-new Vue({
+$(function(){
+	var app=new Vue({
 	el:'.home_center',
 	data:{
-		curr:1
+		// 首页页面
+		curr:1,
+		// 技能页面
+		text:'',
+		select:'en',
+		translateText:'',
+		cityList:[
+			{
+				label:'英国',
+				value:'en'
+			},
+			{
+				label:'日本',
+				value:'fa'
+			},
+		]
 	},
 	mounted:function(){
 		var that = this;
@@ -18,7 +34,21 @@ new Vue({
         //改变导航背景色
         changeBack:function(num){
         	this.curr=num;
-		}
+		},
+		// 翻译
+		translate:function(){
+			console.log(this.text);
+			console.log(this.select);
+			this.translateText=this.text;
+			// this.$http.get('http://openapi.baidu.com/public/2.0/bmt/translate').then(function(res){
+			// 	console.log(res)
+			// })
+		},
 	},
 
+	});
 });
+
+
+
+ 
